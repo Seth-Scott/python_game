cc2a8bd05af1dc3bfc921dc2c7597c37882213bc
Seth Scott <s.scottvancouverwa@gmail.com>
2022-02-26 10:42:01 -0800

    add high score feature, update gitignore

--- main.py
+++ main.py
@@ -37,14 +37,13 @@ while game_on:
 
     # detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_on = False
-        score.game_over()
+        score.reset()
+        snake.reset()
 
     # detect collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_on = False
-            score.game_over()
+            score.reset()
 
     # if head collides with any segment in the tail
 
